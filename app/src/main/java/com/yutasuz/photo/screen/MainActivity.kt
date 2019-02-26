3adc5b9a4cb0a52f68d593143f026d02c3bbe40f
yuta suzuki <banquet.of.merry.widow.y.s@gmail.com>
2019-02-26 20:30:11 +0900

    余分な記述を破棄

--- app/src/main/java/com/yutasuz/photo/screen/MainActivity.kt
+++ app/src/main/java/com/yutasuz/photo/screen/MainActivity.kt
@@ -1,23 +1,11 @@
 package com.yutasuz.photo.screen
 
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
 import com.yutasuz.photo.R
-import com.yutasuz.photo.api.response.FlickrPhotoResponse
-import com.yutasuz.photo.screen.photolist.PhotoListFragment
-
 import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.dsl.module.Module
 
 class MainActivity : AppCompatActivity(){
-
-    private val module: Module = org.koin.dsl.module.applicationContext {
-        factory {
-
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
