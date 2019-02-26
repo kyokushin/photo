e3bb57077d36046e58485ab22d1d870e48b4a3eb
yuta suzuki <banquet.of.merry.widow.y.s@gmail.com>
2019-02-26 16:19:16 +0900

    テストを追加

--- app/src/androidTest/java/com/yutasuz/photo/screen/photolist/PhotoListFragmentTest.kt
+++ app/src/androidTest/java/com/yutasuz/photo/screen/photolist/PhotoListFragmentTest.kt
@@ -2,10 +2,7 @@ package com.yutasuz.photo.screen.photolist
 
 import androidx.fragment.app.testing.launchFragment
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import io.mockk.mockk
-import io.mockk.spyk
-import io.mockk.verify
-import io.mockk.verifySequence
+import io.mockk.*
 import org.junit.After
 import org.junit.Before
 import org.junit.BeforeClass
@@ -40,6 +37,11 @@ class PhotoListFragmentTest : KoinTest {
                 spyk(PhotoListPresenter(view, get()))
             }
 
+            factory(override = true) {
+                val photoRequestState = spyk(PhotoRequestState(get()), recordPrivateCalls = true)
+                photoRequestState
+            }
+
             single<PhotoListContract.Repository>(override = true) {
                 spyk(PhotoListRepository())
             }
@@ -65,6 +67,11 @@ class PhotoListFragmentTest : KoinTest {
                 spyk(PhotoListPresenter(view, get()), recordPrivateCalls = true)
             }
 
+            factory(override = true) {
+                val photoRequestState = spyk(PhotoRequestState(get()), recordPrivateCalls = true)
+                photoRequestState
+            }
+
             single<PhotoListContract.Repository>(override = true) {
                 spyk(PhotoListRepository(), recordPrivateCalls = true)
             }
@@ -74,8 +81,41 @@ class PhotoListFragmentTest : KoinTest {
 
         scenario.onFragment {
             val mockPresenter = it.presenter
-            verify(exactly = 1) {
+            verifyOrder {
                 mockPresenter["requestFirstPageIfNotRequested"]()
+                mockPresenter["requestFirstPage"]()
+                mockPresenter["request"](1)
+            }
+        }
+    }
+
+    @Test
+    fun PhotoListFragment起動時にリクエスト済みならPresenterがrequestを呼ばない() {
+        loadKoinModules(listOf(module(override = true) {
+            factory<PhotoListContract.Presenter>(override = true) { (view: PhotoListContract.View) ->
+                spyk(PhotoListPresenter(view, get()), recordPrivateCalls = true)
+            }
+
+            factory(override = true) {
+                val photoRequestState = spyk(PhotoRequestState(get()), recordPrivateCalls = true)
+
+                every{ photoRequestState.requested } returns true
+                photoRequestState
+            }
+
+            single<PhotoListContract.Repository>(override = true) {
+                spyk(PhotoListRepository(), recordPrivateCalls = true)
+            }
+        }))
+
+        val scenario = launchFragment<PhotoListFragment>()
+
+        scenario.onFragment {
+            val mockPresenter = it.presenter
+            verify(exactly = 1){
+                mockPresenter["requestFirstPageIfNotRequested"]()
+            }
+            verify(exactly = 0){
                 mockPresenter["request"](1)
                 mockPresenter["requestFirstPage"]()
             }
